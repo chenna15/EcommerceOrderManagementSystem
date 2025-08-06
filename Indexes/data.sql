3e431b3c4c7355b3717d7b23d556d005fc93d277
Matham Chenna Basava <109084615+chenna15@users.noreply.github.com>
2025-08-06 15:20:37 +0530

    Update data.sql

--- Indexes/data.sql
+++ Indexes/data.sql
@@ -1,1 +1,18 @@
 
+-- INDEXES--
+
+CREATE INDEX idx_order_date ON orders(order_date);
+ EXPLAIN SELECT * FROM orders WHERE order_date >= '2025-01-01';
+ SHOW INDEX FROM orders;
+
+
+CREATE INDEX idx_user_id ON orders(user_id);
+EXPLAIN SELECT * FROM orders WHERE user_id = 101;
+
+
+CREATE INDEX idx_product_id ON order_items(product_id);
+EXPLAIN SELECT * FROM order_items WHERE product_id = 3;
+
+
+CREATE INDEX idx_payment_status ON payments(status);
+ EXPLAIN SELECT * FROM payments WHERE status = 'Paid';
